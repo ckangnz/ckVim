3a8cba9c655e2d97b387ad409f6d80ab93669c84
Chris Kang(Xero) <chris.kang@xero.com>
2023-10-10 13:31:57 +1300

    Add extra brew packages

--- install_others.sh
+++ install_others.sh
@@ -8,7 +8,10 @@ brew_install_cask "rectangle"
 brew_install_cask "maccy"
 
 #brew_install_cask "azure-data-studio"
+#brew_install_cask "session-manager-plugin"
+#brew_install_cask "microsoft-remote-desktop"
 
 brew_install "bat"
+brew_install "awscli"
 brew_install "kubectl"
 brew_install "helm"
