e37f18052c2c0c2ba5831df97ade5ae504c42c9a
Chris Kang(Xero) <chris.kang@xero.com>
2024-01-10 10:57:31 +1300

    Update neovide configuration

--- .config/nvim/lua/gui/neovide.lua
+++ .config/nvim/lua/gui/neovide.lua
@@ -1,16 +1,10 @@
 if vim.g.neovide then
-  local alpha = function()
-    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.95)))
-  end
-
-  vim.g.neovide_transparency = 0.0
-  vim.g.neovide_background_color = "#1d2021" .. alpha()
-
   vim.g.neovide_input_use_logo = 1
   vim.g.neovide_floating_blur_amount_x = 20
   vim.g.neovide_floating_blur_amount_y = 20
   vim.g.neovide_scroll_animation_length = 0.3
   vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_remember_window_size = true
   vim.g.neovide_cursor_vfx_mode = "wireframe"
   vim.g.neovide_fullscreen = false
 
