8b92078ccc23522a5389c8b543ecf9d09a3d4b62
Chris Kang(Xero-MacM3) <chris.kang@xero.com>
2025-04-30 13:22:41 +1200

    Set neovide fontsize

--- .config/nvim/lua/gui/neovide.lua
+++ .config/nvim/lua/gui/neovide.lua
@@ -1,10 +1,12 @@
 if vim.g.neovide then
+  vim.g.neovide_scale_factor = 1.3 --font size
   vim.g.neovide_input_use_logo = 1
   vim.g.neovide_floating_blur_amount_x = 20
   vim.g.neovide_floating_blur_amount_y = 20
   vim.g.neovide_scroll_animation_length = 0.3
   vim.g.neovide_cursor_antialiasing = true
   vim.g.neovide_remember_window_size = true
+  vim.g.neovide_input_macos_option_key_is_meta = 'both'
   vim.g.neovide_cursor_vfx_mode = "wireframe"
   vim.g.neovide_fullscreen = false
 
