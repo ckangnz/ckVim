b00511f03a63be6b17dc3a8ae753fe1ce7b1f925
Chris Kang(Xero-MacM3) <chris.kang@xero.com>
2025-07-17 11:18:58 +1200

    Fix codecompanion keymaps and coc tab issue

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -5,7 +5,25 @@ require("codecompanion").setup({
         name = "copilot",
         model = 'claude-sonnet-4' -- 'gpt-4'
       },
-      opts = { completion_provider = "coc" }
+      opts = { completion_provider = "coc" },
+      slash_commands = {
+        ["file"] = {
+          keymaps = {
+            modes = {
+              i = "<C-p>",
+              n = { "<C-p>" },
+            },
+          },
+        },
+        ["buffer"] = {
+          keymaps = {
+            modes = {
+              i = "<C-b>",
+              n = { "<C-b>" },
+            },
+          },
+        },
+      }
     },
     inline = {
       adapter = {
@@ -24,7 +42,7 @@ require("codecompanion").setup({
   }
 })
 
-vim.keymap.set('n', '<M-o>', function() vim.cmd('CodeCompanionChat') end, { desc = 'Open CodeCompanion Chat' })
+vim.keymap.set('n', '<M-o>', function() vim.cmd('CodeCompanionChat Toggle') end, { desc = 'Open CodeCompanion Chat' })
 vim.keymap.set('n', '<M-p>', ':CodeCompanion ', { desc = 'Open CodeCompanion Prompt' })
 vim.keymap.set('v', '<M-p>', ':CodeCompanion ', { desc = 'Open CodeCompanion Prompt with Selection' })
 vim.keymap.set('n', '<M-i>', function() vim.cmd('CodeCompanionAction') end, { desc = 'Open CodeCompanion Prompt' })
