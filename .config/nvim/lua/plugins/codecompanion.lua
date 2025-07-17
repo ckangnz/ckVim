4c2f58b453817a0502d32f21607177eef8553f6b
Chris Kang(Xero-MacM3) <chris.kang@xero.com>
2025-07-18 11:46:25 +1200

    update codecompanion.lua

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -3,24 +3,28 @@ require("codecompanion").setup({
     chat = {
       adapter = {
         name = "copilot",
-        model = 'claude-sonnet-4' -- 'gpt-4'
+        -- "claude-3.7-sonnet", "gemini-2.0-flash-001", "claude-opus-4", "claude-3.7-sonnet-thought", "o3-mini", "gpt-4.1", "gpt-4o",
+        model = "claude-sonnet-4",
       },
-      opts = { completion_provider = "coc" },
+      roles = {
+        llm = function(adapter)
+          return "🤖 CodeCompanion (" .. adapter.formatted_name .. ")"
+        end,
+        user = "👤 You",
+      },
+      opts = {
+        goto_file_action = 'tabnew', -- press gR
+        completion_provider = "coc",
+      },
       slash_commands = {
         ["file"] = {
           keymaps = {
-            modes = {
-              i = "<C-p>",
-              n = { "<C-p>" },
-            },
+            modes = { n = { "<C-p>" }, },
           },
         },
         ["buffer"] = {
           keymaps = {
-            modes = {
-              i = "<C-b>",
-              n = { "<C-b>" },
-            },
+            modes = { n = { "<C-b>" }, },
           },
         },
       }
@@ -28,14 +32,14 @@ require("codecompanion").setup({
     inline = {
       adapter = {
         name = "copilot",
-        model = 'claude-sonnet-4' -- 'gpt-4'
+        model = 'claude-sonnet-4'
       },
       opts = { completion_provider = "coc" }
     },
     cmd = {
       adapter = {
         name = "copilot",
-        model = 'claude-sonnet-4' -- 'gpt-4'
+        model = 'claude-sonnet-4'
       },
       opts = { completion_provider = "coc" }
     }
@@ -43,10 +47,25 @@ require("codecompanion").setup({
   extensions = {
     spinner = {},
   },
-
 })
 
-vim.keymap.set('n', '<M-o>', function() vim.cmd('CodeCompanionChat Toggle') end, { desc = 'Open CodeCompanion Chat' })
-vim.keymap.set('n', '<M-p>', ':CodeCompanion ', { desc = 'Open CodeCompanion Prompt' })
-vim.keymap.set('v', '<M-p>', ':CodeCompanion ', { desc = 'Open CodeCompanion Prompt with Selection' })
-vim.keymap.set('n', '<M-i>', function() vim.cmd('CodeCompanionAction') end, { desc = 'Open CodeCompanion Prompt' })
+vim.keymap.set('n', '<M-o>', function() vim.cmd('CodeCompanionChat') end, { desc = 'Start a new CodeCompanion Chat' })
+vim.keymap.set('n', '<BS>', function() vim.cmd('CodeCompanionChat Toggle') end, { desc = 'Toggle CodeCompanion Chat' })
+vim.keymap.set('v', '<BS>', function()
+  vim.cmd('CodeCompanionChat Add')
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', false)
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    local buf_name = vim.api.nvim_buf_get_name(buf)
+    if buf_name:match('CodeCompanion') then
+      local wins = vim.fn.win_findbuf(buf)
+      if #wins > 0 then
+        vim.api.nvim_set_current_win(wins[1])
+        break
+      end
+    end
+  end
+end, { desc = 'Add visual selection to CodeCompanion Chat' })
+
+vim.keymap.set({ 'n', 'v' }, '<M-p>', ':CodeCompanion ', { desc = 'Open CodeCompanion Prompt' })
+vim.keymap.set({ 'n', 'v' }, '<M-i>', function() vim.cmd('CodeCompanionAction') end,
+  { desc = 'Open CodeCompanion Prompt' })
