264ee613edc25ac1801ce75a339ef3d42b3adb03
Chris Kang(Xero-MacM3) <chris.kang@xero.com>
2025-07-25 14:25:16 +1200

    Add help in treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -19,6 +19,7 @@ require('nvim-treesitter.configs').setup {
   sync_install = false,
   auto_install = true,
   ensure_installed = {
+    'help',
     -- Version control
     'gitignore',
     'git_config',
