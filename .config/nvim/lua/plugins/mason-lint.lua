d472eb945e39f05cf55280dd52db3a685e281b9e
Chris Kang(Xero-MacM3) <chris.kang@xero.com>
2025-07-30 10:35:29 +1200

    Remove autocmd in mason-lint that doesn't really do anything

--- .config/nvim/lua/plugins/mason-lint.lua
+++ .config/nvim/lua/plugins/mason-lint.lua
@@ -2,7 +2,9 @@ require("mason-nvim-lint").setup({
   ensure_installed = {
     "shellcheck",
     "markdownlint",
+
     "eslint_d",
+
     "jsonlint",
     "yamllint",
     "hadolint",
@@ -23,11 +25,3 @@ lint.linters_by_ft = {
   yaml = { "yamllint" },
   dockerfile = { "hadolint" },
 }
-
--- Auto-run linting on save and text change
-vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-  group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-  callback = function()
-    lint.try_lint()
-  end,
-})
