5f241d3eda8279320d2b889258209b18e679431a
Chris Kang(Xero-MacM3) <chris.kang@xero.com>
2025-07-30 10:49:06 +1200

    Update lua formatter

--- .config/nvim/lua/plugins/mason-conform.lua
+++ .config/nvim/lua/plugins/mason-conform.lua
@@ -1,54 +1,65 @@
-require("mason-conform").setup({
+require('mason-conform').setup({
   ensure_installed = {
-    "stylua",
-    "prettier",
-    "prettierd",
-    "markdownlint",
-    "codespell",
+    'codespell',
+    'stylua',
+    'markdownlint',
+
+    'prettier',
+    'prettierd',
   },
   ignore_install = {},
 })
 
-require("conform").setup({
+require('conform').setup({
   formatters_by_ft = {
-    lua = { "stylua" },
-    markdown = { "markdownlint" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-    json = { "prettier" },
-    html = { "prettier" },
-    css = { "prettier" },
-    scss = { "prettier" },
-    yaml = { "prettier" },
-    ["*"] = { "codespell" },
-    ["_"] = { "trim_whitespace" },
+    lua = { 'stylua' },
+    markdown = { 'markdownlint' },
+    javascript = { 'prettierd', 'prettier', stop_after_first = true },
+    typescript = { 'prettierd', 'prettier', stop_after_first = true },
+    javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+    typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+    json = { 'prettier' },
+    html = { 'prettier' },
+    css = { 'prettier' },
+    scss = { 'prettier' },
+    yaml = { 'prettier' },
+    ['*'] = { 'codespell' },
+    ['_'] = { 'trim_whitespace' },
   },
   default_format_opts = {
-    lsp_format = "fallback",
+    lsp_format = 'fallback',
   },
   format_on_save = {
-    lsp_format = "fallback",
+    lsp_format = 'fallback',
     timeout_ms = 500,
   },
   notify_on_error = true,
   notify_no_formatters = true,
   formatters = {
     stylua = {
-      args = { "--indent-type", "Spaces", "--indent-width", "2", "--column-width", "100", "-" },
+      args = {
+        '--indent-type',
+        'Spaces',
+        '--indent-width',
+        '2',
+        '--column-width',
+        '100',
+        '--quote-style',
+        'ForceSingle',
+        '-',
+      },
     },
     prettier = {
       args = {
-        "--stdin-filepath",
-        "$FILENAME",
-        "--semi=true",
-        "--quote-props=as-needed",
-        "--jsx-single-quote=false",
-        "--trailing-comma=es5",
-        "--single-quote=false",
-        "--print-width=100"
-      }
-    }
-  }
+        '--stdin-filepath',
+        '$FILENAME',
+        '--semi=true',
+        '--quote-props=as-needed',
+        '--jsx-single-quote=false',
+        '--trailing-comma=es5',
+        '--single-quote=false',
+        '--print-width=100',
+      },
+    },
+  },
 })
