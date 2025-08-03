d30477a8c020d4868e5f9bba77ca20f33f2d14d8
Chris Kang(Xero-MacM3) <chris.kang@xero.com>
2025-08-04 11:19:24 +1200

    Remove nerdcommenter. Use default gcc to comment now :)

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -499,21 +499,6 @@ local plugins = {
     end,
   },
   {
-    'scrooloose/nerdcommenter',
-    event = { 'BufReadPost' },
-    config = function()
-      vim.g.NERDCreateDefaultMappings = 1
-      vim.g.NERDSpaceDelims = 1
-      vim.g.NERDCompactSexyComs = 1
-      vim.g.NERDDefaultAlign = 'left'
-      vim.g.NERDAltDelims_java = 1
-      vim.g.NERDCustomDelimiters = { c = { left = '/**', right = '*/' } }
-      vim.g.NERDCommentEmptyLines = 1
-      vim.g.NERDTrimTrailingWhitespace = 1
-      vim.g.NERDToggleCheckAllLines = 1
-    end,
-  },
-  {
     'skywind3000/asyncrun.vim',
     dependencies = {
       'rcarriga/nvim-notify',
