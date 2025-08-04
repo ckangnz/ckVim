9599be4c982d103f6868930a0167ad30270d2289
Chris Kang(Xero-PC) <chris.kang@xero.com>
2025-08-05 10:27:23 +1200

    Add zsh formatter

--- .config/nvim/lua/plugins/mason-conform.lua
+++ .config/nvim/lua/plugins/mason-conform.lua
@@ -3,6 +3,7 @@ require('mason-conform').setup()
 require('conform').setup({
   formatters_by_ft = {
     lua = { 'stylua' },
+    zsh = { 'beautysh' },
     javascript = { 'prettierd', 'prettier', stop_after_first = true },
     typescript = { 'prettierd', 'prettier', stop_after_first = true },
     javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
