b35949586cd60e089a7b600085386efe2c39b69a
Chris Kang(Xero-PC) <chris.kang@xero.com>
2025-08-07 11:42:54 +1200

    Fix treesitter error issue on deleting the last line

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,17 @@
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-
-vim.api.nvim_create_autocmd('BufReadPost', {
+vim.api.nvim_create_autocmd('FileType', {
   pattern = '*',
   callback = function()
-    vim.cmd('normal! zx')
+    if require('nvim-treesitter.parsers').has_parser() then
+      vim.wo.foldmethod = 'expr'
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.defer_fn(function()
+        if vim.api.nvim_buf_is_valid(0) then
+          vim.cmd('normal! zx')
+        end
+      end, 100)
+    end
   end,
-  desc = 'Expand folds when opening files',
+  desc = 'Setup treesitter folding for supported filetypes',
 })
 
 require('nvim-treesitter.configs').setup({
@@ -14,6 +19,13 @@ require('nvim-treesitter.configs').setup({
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
+    disable = function(_, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
   sync_install = false,
   auto_install = true,
@@ -79,7 +91,7 @@ require('nvim-treesitter.configs').setup({
   indent = {
     enable = true,
   },
-  incremental_selection = { enable = false, },
+  incremental_selection = { enable = false },
 })
 
 if vim.fn.has('unix') == 1 and vim.fn.has('mac') == 1 then
