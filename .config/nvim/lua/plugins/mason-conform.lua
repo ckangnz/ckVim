8428a2f1fbdf08e3425603726cf6c41a8a25c647
Chris Kang (AtlassianM4Pro) <chris.sm.kang542@gmail.com>
2025-10-17 11:20:35 +1300

    Fix format on save and have :Format to format with cmd

--- .config/nvim/lua/plugins/mason-conform.lua
+++ .config/nvim/lua/plugins/mason-conform.lua
@@ -17,6 +17,10 @@ require('conform').setup({
     ['_'] = { 'trim_whitespace' },
   },
   default_format_opts = { timeout_ms = 500 },
+  format_on_save = {
+    lsp_format = 'fallback',
+    timeout_ms = 500,
+  },
   notify_on_error = true,
   notify_no_formatters = true,
   formatters = {
@@ -47,3 +51,15 @@ require('conform').setup({
     },
   },
 })
+
+vim.api.nvim_create_user_command('Format', function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ['end'] = { args.line2, end_line:len() },
+    }
+  end
+  require('conform').format({ async = true, lsp_fallback = true, range = range })
+end, { range = true })
