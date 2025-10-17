26f1c5c2c1202488e6e58ce41379ef61711a74d2
Chris Kang (AtlassianM4Pro) <chris.sm.kang542@gmail.com>
2025-10-17 19:26:32 +1300

    Fix rooter. Find with more obvious pointers

--- .config/nvim/lua/plugins/vim-rooter.lua
+++ .config/nvim/lua/plugins/vim-rooter.lua
@@ -1,4 +1,4 @@
 vim.g.rooter_silent_chdir = 1
-vim.g.rooter_patterns = { '.vscode', '.git', 'node_modules/' }
+vim.g.rooter_patterns = { '.vscode', '.git', '.nvmrc', '.npmrc' }
 vim.g.rooter_resolve_links = 1
 vim.g.rooter_change_directory_for_non_project_files = ''
