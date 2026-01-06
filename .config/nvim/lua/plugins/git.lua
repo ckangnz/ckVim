99de805b5f0f4d0f0e600163dcf874b5fe601d5f
Chris Kang (AtlassianM4Pro) <ckang2@atlassian.com>
2026-01-07 10:56:45 +1300

    Fix ,@ git_bcommits to use FzfLua

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -11,7 +11,7 @@ vim.keymap.set(
 vim.keymap.set(
   'n',
   '<leader>@',
-  ':Telescope git_bcommits<CR>',
+  ':FzfLua git_bcommits<CR>',
   { silent = true, desc = 'Git buffer commits' }
 )
 vim.keymap.set('v', '<leader>2', ':GV!<CR>', { silent = true, desc = 'Git log for selection' })
